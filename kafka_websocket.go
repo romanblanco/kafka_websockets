80201d37328e0b4e00cc3f1fd169fc54495950f7
Roman Blanco <rblanco@redhat.com>
2019-10-07 13:04:49 +0000

    Client config out of for loop

--- kafka_websocket.go
+++ kafka_websocket.go
@@ -8,15 +8,81 @@ import (
 )
 
 func main() {
+  // set a kafka topic to follow
+  topic := "test"
+
+  kafkaConfig := sarama.NewConfig()
+  kafkaConfig.Consumer.Return.Errors = true
+  kafkaConfig.Producer.RequiredAcks = sarama.WaitForAll
+  kafkaConfig.Producer.Retry.Max = 5
+  kafkaConfig.Producer.Return.Successes = true
+
+  brokers := []string{"localhost:9092"}
+
+  // create a websocket token
+  token, _ := philote.NewToken(
+    "roman",
+    []string{"read-write-channel"},
+    []string{"read-write-channel"},
+  )
+  fmt.Printf("philote token created: %T\n", token)
+
+  fmt.Println(token)
+
+  // create a websocket client
+  c1, _ := philote.NewClient("ws://localhost:6380", token)
+  fmt.Printf("philote client 1 created: %T\n", c1)
+
+  c2, _ := philote.NewClient("ws://localhost:6380", token)
+  fmt.Printf("philote client 2 created: %T\n", c2)
+
+  // create a new producer
+  producer, err := sarama.NewSyncProducer(brokers, kafkaConfig)
+  if err != nil {
+    panic(err)
+  }
+  fmt.Printf("kafka producer created: %T\n", producer)
+
+  defer func() {
+    if err := producer.Close(); err != nil {
+      panic(err)
+    }
+  }()
 
   for {
-    kafkaConfig := sarama.NewConfig()
-    kafkaConfig.Consumer.Return.Errors = true
-    kafkaConfig.Producer.RequiredAcks = sarama.WaitForAll
-    kafkaConfig.Producer.Retry.Max = 5
-    kafkaConfig.Producer.Return.Successes = true
+    fmt.Println("********************************")
 
-    brokers := []string{"localhost:9092"}
+    // produce a kafka message
+    go func() {
+      kafka_message := time.Now().String() + ": " + "Kafka says hi!"
+      msg := &sarama.ProducerMessage{
+        Topic: topic,
+        Value: sarama.StringEncoder(kafka_message),
+      }
+      partition, offset, err := producer.SendMessage(msg)
+      if err != nil {
+        panic(err)
+      }
+      fmt.Printf("kafka >: p: %v, o: %v, m: %v\n",
+        partition,
+        offset,
+        kafka_message)
+    }()
+
+    // publish a websocket message
+    go func() {
+      websocket_message := time.Now().String() + ": " + "Websockets say hi!"
+      time.Sleep(2*time.Second)
+      c1.Publish(
+        &philote.Message{
+          Channel: "read-write-channel",
+          Data: websocket_message,
+        })
+      if err != nil {
+        panic(err)
+      }
+      fmt.Println("websocket(c1) >: ", websocket_message)
+    }()
 
     // create a new consumer
     master, err := sarama.NewConsumer(brokers, kafkaConfig)
@@ -31,35 +97,8 @@ func main() {
       }
     }()
 
-    // create a new producer
-    producer, err := sarama.NewSyncProducer(brokers, kafkaConfig)
-    if err != nil {
-      panic(err)
-    }
-    fmt.Printf("kafka producer created: %T\n", producer)
-
-    defer func() {
-      if err := producer.Close(); err != nil {
-        panic(err)
-      }
-    }()
-
-    // set a kafka topic to follow
-    topic := "test"
-
-    // produce a kafka message
-    msg := &sarama.ProducerMessage{
-      Topic: topic,
-      Value: sarama.StringEncoder("Something Cool"),
-    }
-    partition, offset, err := producer.SendMessage(msg)
-    if err != nil {
-      panic(err)
-    }
-    fmt.Printf("kafka message produced: p: %v, o: %v\n", partition, offset)
-
     // consume a kafka message
-    consumer, err := master.ConsumePartition(topic, 0, sarama.OffsetOldest)
+    consumer, err := master.ConsumePartition(topic, 0, sarama.OffsetNewest)
     if err != nil {
       panic(err)
     }
@@ -67,45 +106,27 @@ func main() {
       case err := <-consumer.Errors():
         fmt.Println(err)
       case msg := <-consumer.Messages():
-        fmt.Println("kafka message consumed: ", string(msg.Key), string(msg.Value))
+        fmt.Println("kafka <: ", string(msg.Key), string(msg.Value))
+        // redirect the kafka message to websocket channel
+        time.Sleep(2*time.Second)
+        c1.Publish(
+          &philote.Message{
+            Channel: "read-write-channel",
+            Data: "K: " + string(msg.Value),
+          })
+        if err != nil {
+          panic(err)
+        }
+        fmt.Println("kafka > websocket(c1): ", string(msg.Value))
     }
-
-    // create a websocket token
-    token, _ := philote.NewToken(
-      "roman",
-      []string{"read-write-channel"},
-      []string{"read-write-channel"},
-    )
-    fmt.Printf("philote token created: %T\n", token)
-
-    fmt.Println(token)
-
-    // create a websocket client
-    c1, _ := philote.NewClient("ws://localhost:6380", token)
-    fmt.Printf("philote client 1 created: %T\n", c1)
-
-    c2, _ := philote.NewClient("ws://localhost:6380", token)
-    fmt.Printf("philote client 2 created: %T\n", c2)
-
-    // publish a websocket message
-    go func() {
-      time.Sleep(2*time.Second)
-      c1.Publish(
-        &philote.Message{
-          Channel: "read-write-channel",
-          Data: "My message to the world",
-        })
-      if err != nil {
-        panic(err)
-      }
-      fmt.Println("c1: philote message sent")
-    }()
 
     // receive a websocket message
     message, err := c2.Receive()
     if err != nil {
       panic(err)
     }
-    fmt.Printf("c2: philote message received: %v\n", message.Data)
+    fmt.Printf("websocket(c2) <: %v\n", message.Data)
+
+    fmt.Println("********************************")
   }
 }
